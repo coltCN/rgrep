49f79bde2f9acb2e12deb00534834e3b3c717c75
majf <majf.colt@gmail.com>
2021-11-03 14:26:45 +0800

    实现 Step 1:给定一个字符串以及一个文件，打印出文件中所有包含该字符串的行

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Lines},
+};
+
+use anyhow::Result;
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap[version = "1.0", author = "coltCN"]]
+struct Opts {
+    /// 需要查找的单词
+    word: String,
+    /// 目标文件
+    file: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    // println!("{:?}", opts);
+
+    match read_line(&opts.file) {
+        Ok(lines) => match_word(lines, &opts.word),
+        Err(e) => println!("Err:{}", e),
+    }
+}
+
+fn read_line(filename: &str) -> Result<Lines<BufReader<File>>> {
+    let f = File::open(filename)?;
+    Ok(BufReader::new(f).lines())
+}
+
+fn match_word(lines: Lines<BufReader<File>>, word: &str) {
+    let mut line_num = 0;
+    for line in lines {
+        line_num += 1;
+        if let Ok(l) = line {
+            if let Some(col) = l.find(word) {
+                println!("{}:{}\t {}", line_num, col, l.trim());
+            }
+        }
+    }
 }
